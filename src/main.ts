42c50f2ec38c23c1904958da2b35d020adfe3492
Tornyi Dénes <tornyi.denes@gmail.com>
2014-06-12 21:37:44 +0200

    Remove window object prefix

--- src/main.ts
+++ src/main.ts
@@ -38,8 +38,10 @@
  */
 
 module Talk {
-    declare var navigator: any;
-    declare var window: any;
+    declare var mozRTCSessionDescription;
+    declare var mozRTCPeerConnection;
+    declare var mozRTCIceCandidate;
+    declare var navigator;
 
     export interface Packet {
         length: number;
@@ -60,10 +62,10 @@ module Talk {
         log: (...args: any[]) => void;
     }
 
-    export var PeerConnection = window.RTCPeerConnection || window.webkitRTCPeerConnection || window.mozRTCPeerConnection;
-    export var SessionDescription = window.RTCSessionDescription || window.mozRTCSessionDescription;
-    export var IceCandidate = window.RTCIceCandidate || window.mozRTCIceCandidate;
-    export var MediaStream = window.MediaStream || window.webkitMediaStream;
+    export var PeerConnection = RTCPeerConnection || webkitRTCPeerConnection || mozRTCPeerConnection;
+    export var SessionDescription = RTCSessionDescription || mozRTCSessionDescription;
+    export var IceCandidate = RTCIceCandidate || mozRTCIceCandidate;
+    export var MediaStream = MediaStream || webkitMediaStream;
 
     export var userMedia: any;
 
@@ -173,8 +175,8 @@ module Talk {
      */
 
     export function attachMediaStream(element: HTMLVideoElement, stream: MediaStream): HTMLVideoElement {
-        if(window.URL) {
-            element.src = window.URL.createObjectURL(stream);
+        if(URL) {
+            element.src = URL.createObjectURL(stream);
         }
         else {
             element.src = <any> stream;
